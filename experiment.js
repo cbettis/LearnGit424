f58c0c1cf1741fe435ed15b616fc2bc6f778d738
Cbettis <cbettis@wisc.edu>
2024-02-20 15:22:02 -0600

    experiment

--- experiment.js
+++ experiment.js
@@ -1,1 +1,10 @@
 // commenting out
+
+// a commit involves some changes
+// Steps to do this part correctly:
+// 1. go to settings command palette and then git:create branch in the main folder "LearnGit424", it prompts you for a name
+// 2. put in the name of the branch you want it to be called and then make sure on the lower left side of the screen you see the name of your branch next to the blue little > < thing
+// 3. add in a new js file and make it "experiment.js" put in osme comments and then you can save and then press commit butDO NOT publish branch yet
+// 4. then in the bottom where you made sure the branch name is what was showing, click it and go back to main
+// 5. that js file you created in the other branch should have a line through it or should be missing from the main folder --> this is what you want
+// 6. then you can go to command palette and merge WHILE UNDER THE MAIN view and then select that branch to merge
